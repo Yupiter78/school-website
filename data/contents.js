2d347414972e178912255d338adf5cfc48099334
Yuriy Vedernikov <yu.piter.gm@gmail.com>
2024-01-12 22:23:24 +0300

    fix: change gallery.js to render_data.js

--- data/contents.js
+++ data/contents.js
@@ -1,4 +1,3 @@
-const box = document.querySelector("#custom_box");
 const galleryContent = [
     {
         path: "./images/image-18-10-23-09-09-7.jpeg",
@@ -56,22 +55,6 @@ const galleryContent = [
     }
 ];
 
-const galleryItemTemplates = galleryContent.map(
-    (item) =>
-        `<div class="col">
-      <div class="card shadow-sm">
-        <img src=${item.path} width="100%" height="100%" alt="image"/>
-        <div class="card-body">
-          <p class="card-text">${item.title}</p>
-          <div class="d-flex justify-content-between align-items-center">
-            <div class="btn-group">
-              <button type="button" class="btn btn-sm btn-primary">Просмотр</button>
-              
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>`
-);
-
-box.insertAdjacentHTML("beforeend", galleryItemTemplates.join(""));
+export default {
+    galleryContent
+};
